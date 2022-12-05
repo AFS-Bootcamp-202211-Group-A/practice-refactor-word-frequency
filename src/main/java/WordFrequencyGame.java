fac58a56127275f04cbd9ee1c75e5d7bcf9ada2f
Hezz <heinrich@connect.hku.hk>
2022-12-05 22:57:00 +0800

    refactor: use of java stream api to combine the processes

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,25 +1,19 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr){
 
         try {
-
-            //split the input string with 1 to n pieces of spaces
-            String[] wordArray = inputStr.split("\\s+");
-
-            Map <String, Integer> wordCountMap = new HashMap<>();
-            Arrays.stream(wordArray).forEach(word -> wordCountMap.merge(word, 1, Integer::sum));
-
-            StringJoiner joiner = new StringJoiner("\n");
-
-            wordCountMap.entrySet()
+            return Arrays.stream(inputStr.split("\\s+"))
+                    .collect(Collectors.groupingBy(s -> s)) // group by word -> Entry<word, List<word>>
+                    .entrySet()
                     .stream()
-                    .map(wordCountEntry -> new WordCountInfo(wordCountEntry.getKey(), wordCountEntry.getValue()))
-                    .sorted((w1, w2) -> w2.getWordCount() - w1.getWordCount())
-                    .forEach(wordCountInfo -> joiner.add(wordCountInfo.getWordWithCount()));
+                    .map(entry -> new WordCountInfo(entry.getKey(), entry.getValue().size()))
+                    .sorted(Comparator.comparing(WordCountInfo::getWordCount, Comparator.reverseOrder()))
+                    .map(WordCountInfo::getWordWithCount)
+                    .collect(Collectors.joining("\n"));
 
-            return joiner.toString();
         } catch (Exception e) {
             return "Calculate Error";
         }
