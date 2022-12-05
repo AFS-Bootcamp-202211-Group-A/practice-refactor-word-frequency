53c413a42279fd061deb8a05cf27013fa832d453
Hezz <heinrich@connect.hku.hk>
2022-12-05 22:37:34 +0800

    refactor: use of java stream api to combine the processes

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,13 +11,13 @@ public class WordFrequencyGame {
             Map <String, Integer> wordCountMap = new HashMap<>();
             Arrays.stream(wordArray).forEach(word -> wordCountMap.merge(word, 1, Integer::sum));
 
-            List<WordCountInfo> wordCountList = new ArrayList<>();
-            wordCountMap.forEach((key, value) -> wordCountList.add(new WordCountInfo(key, value)));
-
-            wordCountList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
             StringJoiner joiner = new StringJoiner("\n");
-            wordCountList.forEach(wordCountInfo -> joiner.add(wordCountInfo.getWordWithCount()));
+
+            wordCountMap.entrySet()
+                    .stream()
+                    .map(wordCountEntry -> new WordCountInfo(wordCountEntry.getKey(), wordCountEntry.getValue()))
+                    .sorted((w1, w2) -> w2.getWordCount() - w1.getWordCount())
+                    .forEach(wordCountInfo -> joiner.add(wordCountInfo.getWordWithCount()));
 
             return joiner.toString();
         } catch (Exception e) {
