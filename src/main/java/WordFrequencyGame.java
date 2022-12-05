90984f8462380293169f2fb53705eb91627deecf
Hezz <heinrich@connect.hku.hk>
2022-12-06 00:29:25 +0800

    fix: improve collector usage

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,15 +1,18 @@
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.summingInt;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr){
 
         try {
             return Arrays.stream(inputStr.split("\\s+"))
-                    .collect(Collectors.groupingBy(s -> s)) // group by word -> Entry<word, List<word>>
+                    .collect(Collectors.groupingBy(Function.identity(), summingInt(x -> 1))) // group by word -> Entry<word, List<word>>
                     .entrySet()
                     .stream()
-                    .map(entry -> new WordCountInfo(entry.getKey(), entry.getValue().size()))
+                    .map(entry -> new WordCountInfo(entry.getKey(), entry.getValue()))
                     .sorted(Comparator.comparing(WordCountInfo::getWordCount, Comparator.reverseOrder()))
                     .map(WordCountInfo::getWordWithCount)
                     .collect(Collectors.joining("\n"));
