69062ea6dc02f39550ac239d58a957a5c08990ed
Hezz <heinrich@connect.hku.hk>
2022-12-05 22:17:43 +0800

    refactor: move function logic to getResultBlock

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,21 +8,15 @@ public class WordFrequencyGame {
             //split the input string with 1 to n pieces of spaces
             String[] wordArray = inputStr.split("\\s+");
 
+            Map <String, Integer> wordCountMap = new HashMap<>();
+
+            Arrays.stream(wordArray).forEach(word -> wordCountMap.merge(word, 1, Integer::sum));
+
             List<Input> inputList = new ArrayList<>();
-            for (String word : wordArray) {
-                Input input = new Input(word, 1);
+            for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()){
+                Input input = new Input(entry.getKey(), entry.getValue());
                 inputList.add(input);
             }
-
-            //get the map for the next step of sizing the same word
-            Map<String, List<Input>> map =getListMap(inputList);
-
-            List<Input> list = new ArrayList<>();
-            for (Map.Entry<String, List<Input>> entry : map.entrySet()){
-                Input input = new Input(entry.getKey(), entry.getValue().size());
-                list.add(input);
-            }
-            inputList = list;
 
             inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
@@ -38,26 +32,6 @@ public class WordFrequencyGame {
             return "Calculate Error";
         }
 
-    }
-
-
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getWord())){
-                ArrayList wordArray = new ArrayList<>();
-                wordArray.add(input);
-                map.put(input.getWord(), wordArray);
-            }
-
-            else {
-                map.get(input.getWord()).add(input);
-            }
-        }
-
-
-        return map;
     }
 
 
