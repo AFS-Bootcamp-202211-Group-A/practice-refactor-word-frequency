01afacc4426335517e6d3709febf32ea972c06f3
Marie Chow <chowma3@oocl.com>
2022-12-05 20:38:21 +0800

    refactor: extract countGroupByValueInput in getResult

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,15 +9,9 @@ public class WordFrequencyGame {
             List<Input> inputList = splitInputStringToList(inputStr);
 
             //get the map for the next step of sizing the same word
-            Map<String, List<Input>> groupedInput = groupInputByValue(inputList);
+            Map<String, List<Input>> groupByValueInput = groupInputByValue(inputList);
 
-            List<Input> inputCountList = new ArrayList<>();
-            groupedInput.forEach(((key, value) -> {
-                Input input = new Input(key, value.size());
-                inputCountList.add(input);
-            }));
-
-            inputCountList.sort((currInput, nextInput) -> nextInput.getWordCount() - currInput.getWordCount());
+            List<Input> inputCountList = countGroupByValueInput(groupByValueInput);
 
             StringJoiner joiner = new StringJoiner("\n");
             inputCountList
@@ -28,6 +22,16 @@ public class WordFrequencyGame {
         } catch (Exception e) {
             return "Calculate Error";
         }
+    }
+
+    private List<Input> countGroupByValueInput(Map<String, List<Input>> groupedInput) {
+        List<Input> inputCountList = new ArrayList<>();
+        groupedInput.forEach(((key, value) -> {
+            Input input = new Input(key, value.size());
+            inputCountList.add(input);
+        }));
+        inputCountList.sort((currInput, nextInput) -> nextInput.getWordCount() - currInput.getWordCount());
+        return inputCountList;
     }
 
     //split the input string with 1 to n pieces of spaces
