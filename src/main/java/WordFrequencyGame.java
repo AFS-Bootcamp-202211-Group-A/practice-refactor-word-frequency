4febddd3cc791d47600df02df31253ba020f8379
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-06 10:14:18 +0800

    refactor: chaining all stream api

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,44 +9,23 @@ public class WordFrequencyGame {
 
     public String getResult(String inputStr){
             try {
-                List<Input> inputList = extractInputList(inputStr);
-                inputList = aggregateInputList(inputList);
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-                return printInputList(inputList);
+                return printWordFrequency(inputStr);
             } catch (Exception e) {
                 return "Calculate Error";
             }
     }
 
-    private String printInputList(List<Input> inputList) {
-        return inputList.stream()
-                .map(word -> word.getValue() + " " + word.getWordCount())
-                .collect(Collectors.joining(NEWLINE_CHARACTER));
-    }
-
-    private List<Input> aggregateInputList(List<Input> inputList) {
-        Map<String, List<Input>> map = getListMap(inputList);
-        inputList = map.entrySet()
+    private String printWordFrequency(String inputStr) {
+        String[] arr = inputStr.split(WHITE_SPACE_REGEX);
+        return Arrays.stream(arr)
+                .map(word -> new Input(word, COUNT))
+                .collect(Collectors.groupingBy(Input::getValue))
+                .entrySet()
                 .stream()
                 .map( word ->  new Input(word.getKey(), word.getValue().size()))
-                .collect(Collectors.toList());
-        return inputList;
-    }
-
-    private List<Input> extractInputList(String inputStr) {
-        String[] arr = inputStr.split(WHITE_SPACE_REGEX);
-        List<Input> inputList = Arrays.stream(arr)
-                .map(word -> new Input(word, COUNT))
-                .collect(Collectors.toList());
-        return inputList;
-    }
-
-
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = inputList
-                .stream()
-                .collect(Collectors.groupingBy(Input::getValue));
-        return map;
+                .sorted((w1, w2) -> w2.getWordCount() - w1.getWordCount())
+                .map(word -> word.getValue() + " " + word.getWordCount())
+                .collect(Collectors.joining(NEWLINE_CHARACTER));
     }
 
 
